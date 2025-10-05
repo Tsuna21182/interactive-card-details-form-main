ff76ac17b497f0d50aa3dbc94cb1e82241bb261b
Tsuna21182 <tsunadelanochecer@gmail.com>
2025-10-05 16:31:21 -0500

    correcion maximo digitos

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -8,8 +8,9 @@ type CardProps = {
 
 function Card({ cardName, cardNumb, cardMes, cardYear, cardCVC }: CardProps) {
   const formattedCardNumb = cardNumb
-    .replace(/\s/g, "")
-    .replace(/(\d{4})/g, "$1 ")
+    .replace(/\D/g, "")
+    .slice(0, 16)
+    .replace(/(\d{4})(?=\d)/g, "$1 ")
     .trim();
 
   return (
@@ -20,15 +21,16 @@ function Card({ cardName, cardNumb, cardMes, cardYear, cardCVC }: CardProps) {
         </p>
       </div>
 
-      <div className="bg-[url(/images/bg-card-front.png)] bg-no-repeat bg-cover w-70 h-40 mt-[-4.2rem] absolute rounded-lg p-5 flex flex-col gap-3 justify-end md:w-96 md:h-60 md:mt-[-10.5rem] md:left-70 lg:left-15 lg:mt-[10rem] lg:w-auto xxl:top-1 3xl:top-1 3xl:left-62 3xl:w-xl 3xl:h-80">
+      <div className="bg-[url(/images/bg-card-front.png)] bg-no-repeat bg-cover w-70 h-40 mt-[-4.2rem] absolute rounded-lg p-5 flex flex-col gap-3 justify-end md:w-96 md:h-60 md:mt-[-10.5rem] md:left-70 lg:left-15 lg:mt-[10rem] lg:w-lg xxl:top-1 3xl:top-1 3xl:left-62 3xl:w-xl 3xl:h-80">
         <img
           src="/images/card-logo.svg"
           alt="icono logo"
           className="absolute top-5 left-5 w-14 md:left-5 md:top-6 md:w-20"
         />
-        <p className="text-lg text-White md:text-2xl lg:text-4xl">
+        <p className="text-lg text-White md:text-2xl lg:text-4xl tracking-[0.1em] font-[Space_Grotesk] min-w-[200px]">
           {formattedCardNumb || "0000 0000 0000 0000"}
         </p>
+
         <div className="flex justify-between">
           <p className="font-bold uppercase text-xs text-White md:text-xl">
             {cardName || "Jane Appleseed"}
